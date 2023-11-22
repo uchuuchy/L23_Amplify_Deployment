39ccd64a80cb14a87bc2fc3b10a5420b534eaea8
vsardinas3101 <vsardinas3101@floridapoly.edu>
2023-11-21 19:56:07 -0500

    Added Authentication

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "l23amplifydeployment432a711d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
